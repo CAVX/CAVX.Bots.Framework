01e286fcd37d851c19646cff63d12bc48a03ecf2
Corey Blakeborough <corey@blakeborough.me>
2022-08-21 20:57:15 -0600

    Additional extensions

---
+++ CAVX.Bots.Framework/Extensions/OverwritePermissionsExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAVX.Bots.Framework.Extensions
+{
+    public static class OverwritePermissionsExtensions
+    {
+        public static bool? ViewChannelPermissionDefined(this OverwritePermissions? overwritePermissions)
+        {
+            if (!overwritePermissions.HasValue || overwritePermissions.Value.ViewChannel == PermValue.Inherit)
+                return null;
+
+            return overwritePermissions.Value.ViewChannel == PermValue.Allow;
+        }
+    }
+}
---
+++ CAVX.Bots.Framework/Extensions/SocketThreadChannelExtensions.cs
@@ -0,0 +1,51 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAVX.Bots.Framework.Extensions
+{
+    public enum ThreadChannelRoleAccess
+    {
+        Private,
+        PossibleAccess,
+        Forbidden,
+        Accessible
+    }
+
+    public static class SocketThreadChannelExtensions
+    {
+        public static ThreadChannelRoleAccess CheckRoleAccess(this SocketThreadChannel threadChannel, IRole role)
+        {
+            //Only mods can view private threads.
+            if (threadChannel.IsPrivateThread)
+                return ThreadChannelRoleAccess.Private;
+            else if (role == null)
+                return ThreadChannelRoleAccess.PossibleAccess;
+            else
+            {
+                //First check if a channel is blocked to @everyone, because that makes it "private".
+                bool isPrivate = false;
+                var everyoneRole = role.Guild.EveryoneRole;
+                if (everyoneRole.Id != role.Id)
+                {
+                    isPrivate = !(threadChannel.GetPermissionOverwrite(everyoneRole).ViewChannelPermissionDefined()
+                        ?? threadChannel.ParentChannel.GetPermissionOverwrite(everyoneRole).ViewChannelPermissionDefined()
+                        ?? (threadChannel.ParentChannel as SocketTextChannel)?.Category?.GetPermissionOverwrite(everyoneRole).ViewChannelPermissionDefined()
+                        ?? true);
+                }
+
+                //See if users with the current role can view the threads without the right role access.
+                var noAccess = !(threadChannel.GetPermissionOverwrite(role).ViewChannelPermissionDefined()
+                    ?? threadChannel.ParentChannel.GetPermissionOverwrite(role).ViewChannelPermissionDefined()
+                    ?? (threadChannel.ParentChannel as SocketTextChannel)?.Category?.GetPermissionOverwrite(role).ViewChannelPermissionDefined()
+                    ?? (!isPrivate && role.Permissions.ViewChannel)); //if it's private, you need to explicitly have a channel setting defined.
+
+                return noAccess ? ThreadChannelRoleAccess.Forbidden : ThreadChannelRoleAccess.Accessible;
+            }
+        }
+    }
+}
