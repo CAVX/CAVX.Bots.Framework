41f5b5f486300405046a5af91de2e42a846f9a73
Corey Blakeborough <corey@blakeborough.me>
2023-01-18 01:22:00 -0700

    Additional extension method.

---
+++ CAVX.Bots.Framework/Extensions/IMessageExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAVX.Bots.Framework.Extensions
+{
+    public static class IMessageExtensions
+    {
+        public static bool HasMessageReferenceInSameChannel(this IMessage message)
+            => message.Reference != null && message.Reference.MessageId.IsSpecified && message.Reference.ChannelId == message.Channel.Id;
+    }
+}
