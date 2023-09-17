7888373a18cafc3ddedb86e0820a17aee2cdd70c
Corey Blakeborough <corey@blakeborough.me>
2023-09-17 14:40:37 -0600

    Relative Path

---
+++ CAVX.Bots.Framework/Utilities/RelativePath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAVX.Bots.Framework.Utilities
+{
+    public class RelativePath
+    {
+        public static string Combine(params string[] paths)
+        {
+            paths = paths.Prepend(AppDomain.CurrentDomain.BaseDirectory).ToArray();
+            return Path.Combine(paths);
+        }
+    }
+}
